ffc305f583b627ed71583ec500e803d12bf65b4d
Julio Guirola <julioguirola03@gmail.com>
2026-01-02 13:21:13 -0500

    feat: add Switch component for toggle functionality; integrate with Grafica for sex selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import Grafica from "@/components/grafica";
+import { Switch } from "@/components/ui/switch";
+import { useState } from "react";
 
 export default function Home() {
+  const [sex, setSex] = useState<'M' | 'F'>('M')
+
+
+
   return (
-    <div className="w-svw h-svh flex items-center justify-center">
-      <Grafica sex="M" />
+    <div className="w-svw h-svh flex flex-col items-center justify-center gap-2">
+      <Switch onCheckedChange={value => console.log(value)} />
+      <Grafica sex={sex} />
     </div>
   );
 }
