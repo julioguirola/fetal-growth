61ed01b547ea466b908786a3e230fc4d6f488bde
Julio Guirola <julioguirola03@gmail.com>
2026-01-03 11:32:38 -0500

    nueva prueba de pwa offline

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "next-pwa-v1";
+const OFFLINE_URL = "/";
+
+const STATIC_ASSETS = [
+  "/"
+];
+
+// Instalación
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activación
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
