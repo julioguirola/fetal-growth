761e00dca4d8c77fdc8d792e54560ceb2aa0368c
Julio Guirola <julioguirola03@gmail.com>
2026-01-03 11:50:57 -0500

    dejando para luego lo de offline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,6 @@ import { Label } from "@/components/ui/label";
 export default function Home() {
   const [sex, setSex] = useState<'M' | 'F'>('M')
 
-  useEffect(() => {
-      if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-          navigator.serviceWorker
-            .register('/sw.js')
-            .then(registration => {
-              console.log('Service Worker registrado con éxito:', registration);
-            })
-            .catch(error => {
-              console.error('Error al registrar el Service Worker:', error);
-            });
-        });
-      }
-  }, []);
-
   return (
     <div className="w-svw h-svh flex flex-col items-center justify-center gap-2">
       <Switch onCheckedChange={value => setSex(value ? 'F' : 'M')} id="sex-switch" />
